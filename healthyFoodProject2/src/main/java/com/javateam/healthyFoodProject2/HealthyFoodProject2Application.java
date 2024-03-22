8deaf0c2251aea544ab3695eb8103163a5e7f19a
chaelim lee <cofla1007@gmail.com>
2024-03-22 16:19:00 +0900

    first upload

---
+++ healthyFoodProject2/src/main/java/com/javateam/healthyFoodProject2/HealthyFoodProject2Application.java
@@ -0,0 +1,13 @@
+package com.javateam.healthyFoodProject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyFoodProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyFoodProject2Application.class, args);
+	}
+
+}
